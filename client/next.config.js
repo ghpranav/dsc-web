d07a4f785f85b363a4d2b3c00c5e3a52e9c65552
Pranav Bedre <bedrepranav@gmail.com>
2020-10-21 16:22:17 +0530

    client: Switch to concurrent mode

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,14 @@ const withPWA = require("next-pwa");
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
     disable: prod ? false : true,
     dest: "public",
+  },
+  experimental: {
+    modern: true,
+    reactMode: "concurrent",
+    polyfillsOptimization: true,
   },
 });
